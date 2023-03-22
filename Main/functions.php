011e46f1d0ee1f44bddcd47fe7089c34482846a0
Zajancz <seledka.artem123@gmail.com>
2023-03-22 19:10:36 +0100

    [#4] Added an ability to view, modify, add or remove tags
    Tags were loaded for each film from our database

--- Main/functions.php
+++ Main/functions.php
@@ -3,9 +3,17 @@
 <?php
 function get_all_data(){
     global $conn;
-    #$result = mysqli_query($conn, "SELECT * FROM movies WHERE movieId < 50");
     $result = mysqli_query($conn, "SELECT * FROM movies LIMIT 50");
     while ($row = mysqli_fetch_assoc($result)) {
+        $sql = "SELECT DISTINCT * FROM tags WHERE movieId = " . $row["movieId"];
+        $full_line = mysqli_query($conn, $sql);
+        $tags = "";
+        while ($line = mysqli_fetch_assoc($full_line)) {
+            $position = strpos($tags, $line["tag"]);
+            if ($position === false) {
+                $tags .= $line["tag"] . ", ";
+            }
+        }
         ?>
           <div class="col-4">
             <div class="card shadow-sm">
@@ -20,11 +28,22 @@ function get_all_data(){
                   <div class="card-body">
             
             <center>
-               <?php get_genres($row["genres"]) ?>
+                <?php get_genres($row["genres"]) ?>
+            </center>
+            <center>
+                <?php
+                if ($tags != "") {
+                    view_tags($tags);
+                } else {
+                    ?>
+                    <span class="badge bg-danger">No tags</span>
+                    <?php
+                }
+                ?>
             </center>
 
                     <div class="d-flex justify-content-between align-items-center">
-                        <a name="" id="" class="btn btn-sm btn-outline-secondary" href="update.php?movieId=<?php echo($row["movieId"]) ?>&title=<?php echo ($row["title"]) ?>&genres=<?php echo ($row["genres"])?>" role="button">Update</a>
+                        <a name="" id="" class="btn btn-sm btn-outline-secondary" href="update.php?movieId=<?php echo($row["movieId"]) ?>&title=<?php echo ($row["title"]) ?>&genres=<?php echo ($row["genres"])?>&tags=<?php echo $tags?>" role="button">Update</a>
                         <a name="" id="" class="btn btn-sm btn-outline-secondary" href="delete.php?movieId=<?php echo($row["movieId"]) ?>" role="button">Delete</a>
                     </div>
                     </div>
@@ -35,12 +54,18 @@ function get_all_data(){
     }
 }
 
-function insert_data($title, $genres){
+function insert_data($title, $genres, $insert_tags){
     global $conn;
 
     $sql = "INSERT INTO movies (title, genres) VALUES ('$title', '$genres')";
-    $result = mysqli_query($conn, $sql);
-    if ($result) {
+    $insert_m_result = mysqli_query($conn, $sql);
+
+    $test1 = "SELECT movieId FROM movies WHERE title=$title";
+
+    $insert__t_query = "INSERT INTO tags (movieId, tag) VALUES ('$test1','$insert_tags')";
+    $insert_t_result = mysqli_query($conn, $insert__t_query);
+
+    if ($insert_m_result && $insert_t_result) {
         return true;
     }
     else{
@@ -53,8 +78,12 @@ function delete_movie($movieId)
     global $conn;
 
     $sql = "DELETE FROM movies WHERE movieId=$movieId";
-    $result = mysqli_query($conn, $sql);
-    if ($result) {
+    $delete_m_result = mysqli_query($conn, $sql);
+
+    $delete_tags = "DELETE FROM tags WHERE movieId=$movieId";
+    $delete_t_result = mysqli_query($conn, $delete_tags);
+
+    if ($delete_m_result && $delete_t_result) {
         return true;
     }
     return false;
@@ -74,6 +103,25 @@ function update_movie($movieId, $title, $genres)
     return false;
 
 }
+
+function update_tags($movieId, $new_tags)
+{
+    global $conn;
+    $tags_query = "INSERT INTO tags (movieId, tag) VALUES ('$movieId', '$new_tags')";
+    $tags_result = mysqli_query($conn, $tags_query);
+    $tags_query = "UPDATE tags SET tag='$new_tags' WHERE movieId=$movieId";
+    
+    if($new_tags == " "){
+        $tags_query = "DELETE FROM tags WHERE movieId=$movieId";
+    }
+    
+    $tags_result = mysqli_query($conn, $tags_query);
+    if ($tags_result) {
+        return true;
+    }
+    return false;
+}
+
 
 
 function get_genres($genres)
@@ -95,4 +143,21 @@ function get_genres($genres)
         <?php
     }
 }
+
+function view_tags($tags)
+{
+    if (substr($tags, -2) == ", ") {
+        $tags = substr($tags, 0, -2);
+    }
+    $tags_array = explode(", ", $tags);
+    foreach ($tags_array as $tag) {
+        ?>
+        <span class="badge bg-info text-dark">
+            <?php echo $tag ?>
+        </span>
+        <?php
+    }
+}
+
+
 ?>
--- Main/insert.php
+++ Main/insert.php
@@ -4,10 +4,10 @@
     function error_redirect() {
     header("Location: main.php?error=1");
     }
-if (isset($_POST["title"],$_POST["genres"]) && 
-    !empty($_POST["title"]) && !empty($_POST["genres"]))
+if (isset($_POST["title"],$_POST["genres"], $_POST["tags"]) && 
+    !empty($_POST["title"]) && !empty($_POST["genres"]) && !empty($_POST["tags"]))
     {
-        $result = insert_data($_POST["title"], $_POST["genres"]);
+        $result = insert_data ($_POST["title"], $_POST["genres"], $_POST["tags"]);
         if ($result)
         {
             header("Location: main.php"); 
